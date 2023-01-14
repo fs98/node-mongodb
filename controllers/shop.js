0fe2eb4ad3fb3da05cb835f3859da89c0800c7af
Fata Sefer <sefer.fata@gmail.com>
2023-01-14 22:58:25 +0100

    feat: write methods for getting a product by id

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,17 +16,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
-  Product.findById(prodId)
-    .then((product) => {
+
+  Product.getById(prodId)
+    .then((result) => {
+      const product = result[0];
       res.render("shop/product-detail", {
         product: product,
         pageTitle: product.title,
--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -17,6 +18,24 @@ class Product {
       .then((products) => {
         console.log(products);
         return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static getById(id) {
+    const db = getDb();
+    console.log(id);
+    return db
+      .collection("products")
+      .find({
+        _id: mongodb.ObjectId(id),
+      })
+      .toArray()
+      .then((product) => {
+        console.log(product);
+        return product;
       })
       .catch((err) => {
         console.log(err);
