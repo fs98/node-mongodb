02f38a985837423552a8d550c7f6687c1507e3c7
Fata Sefer <sefer.fata@gmail.com>
2023-01-14 22:38:34 +0100

    refactor: remove unused code

--- controllers/admin.js
+++ controllers/admin.js
@@ -97,17 +97,6 @@ exports.getProducts = (req, res, next) => {
     .catch((err) => {
       console.log(err);
     });
-
-  // req.user
-  //   .getProducts()
-  //   .then(products => {
-  //     res.render('admin/products', {
-  //       prods: products,
-  //       pageTitle: 'Admin Products',
-  //       path: '/admin/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
 };
 
 // exports.postDeleteProduct = (req, res, next) => {
