3ccd11d01a4ad5c2cf8ce3aa4430916f4605e2bf
Fata Sefer <sefer.fata@gmail.com>
2023-01-19 21:59:30 +0100

    feat: define user schema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
+
 // const mongodb = require("mongodb");
 // const { getDb } = require("../util/database");
 
