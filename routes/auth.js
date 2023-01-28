dd94b5e6b7ad877e7445a185262bf4d62b32600f
Fata Sefer <sefer.fata@gmail.com>
2023-01-29 00:04:03 +0100

    feat: register resetting password routes

--- routes/auth.js
+++ routes/auth.js
@@ -15,4 +15,12 @@ router.get("/signup", authController.getSignUp);
 
 router.post("/signup", authController.postSignUp);
 
+router.get("/reset-password", authController.getReset);
+
+router.post("/reset-password", authController.postReset);
+
+router.get("/reset-password/:token", authController.getChangePassword);
+
+router.post("/update-password", authController.postChangePassword);
+
 module.exports = router;
