29aca7941d7e2006ad9695b1e3071a68883c71aa
Fata Sefer <sefer.fata@gmail.com>
2023-01-29 00:07:08 +0100

    fix: fetch only products that belong to logged user

--- controllers/admin.js
+++ controllers/admin.js
@@ -73,7 +73,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user })
     .then((products) => {
       res.render("admin/products", {
         prods: products,
