07e342d0b8b4083e8667e804da5ebb3789f832a7
Fata Sefer <sefer.fata@gmail.com>
2023-01-29 17:32:24 +0100

    feat: validate login

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,12 +25,19 @@ exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.status(422).render("auth/login", {
+      pageTitle: "Login",
+      path: "/login",
+      errorMessage: errors.array(),
+    });
+  }
+
   User.findOne({ email: email })
     .then((user) => {
-      if (!user) {
-        req.flash("error", "Invalid email or password");
-        return res.redirect("/login");
-      }
       bcrypt
         .compare(password, user.password)
         .then((match) => {
--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,25 @@ const router = express.Router();
 // LOGIN
 router.get("/login", authController.getLogin);
 
-router.post("/login", authController.postLogin);
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (!user) {
+            return Promise.reject("User with this email does not exist.");
+          }
+        });
+      }),
+    body("password")
+      .isLength({ min: 5 })
+      .withMessage("Password must be at least 5 characters long"),
+  ],
+  authController.postLogin
+);
 
 router.post("/logout", authController.postLogout);
 
@@ -22,9 +40,7 @@ router.post(
       .isEmail()
       .withMessage("Please enter a valid email.")
       .custom((value, { req }) => {
-        return User.findOne({
-          email: value,
-        }).then((result) => {
+        return User.findOne({ email: value }).then((result) => {
           if (result) {
             return Promise.reject("This user already exists!");
           }
