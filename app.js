80189d346de604696729c4ec5c1d3c4b84ba86a9
Fata Sefer <sefer.fata@gmail.com>
2023-01-31 23:46:36 +0100

    feat: register middleware for handling errors

--- app.js
+++ app.js
@@ -48,11 +48,14 @@ app.use((req, res, next) => {
   if (req.session.user) {
     User.findById(req.session.user._id)
       .then((user) => {
+        if (!user) {
+          return next();
+        }
         req.user = user;
         next();
       })
       .catch((err) => {
-        console.log(err);
+        throw new Error(err);
       });
   } else {
     next();
@@ -69,7 +72,12 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get("/500", errorController.get500);
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.redirect("/500");
+});
 
 mongoose
   .connect(MONGODB_URI)
